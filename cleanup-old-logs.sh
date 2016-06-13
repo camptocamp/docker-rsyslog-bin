75a1d3d4d593d699c290b9a80baf691538e780e6
Marc Fournier <marc.fournier@camptocamp.com>
2016-06-13 15:19:32 +0200

    avoid trying to remove /var/log itself

--- cleanup-old-logs.sh
+++ cleanup-old-logs.sh
@@ -8,7 +8,7 @@ test "$1" -gt "0"
 while true; do
   echo "=> about to remove the following elements, which are older than ${1} days old:"
   find /var/log -type f -mtime "+${1}" -ls -delete
-  find /var/log -empty -ls -delete
+  find /var/log -mindepth 1 -empty -ls -delete
   echo "=> going back to sleep for one hour..."
   sleep 3600
 done
