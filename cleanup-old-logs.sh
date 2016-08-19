d0ffd1912c0a17c850ca86bdfef0446e8ea09ffc
Marc Fournier <marc.fournier@camptocamp.com>
2016-08-19 16:33:15 +0200

    remove oldest logs, as long as disk usage is above a certain threshold

--- cleanup-old-logs.sh
+++ cleanup-old-logs.sh
@@ -5,12 +5,29 @@ set -e
 test -n "$1"
 test "$1" -gt "0"
 
+check_du () {
+  percent="$(df -P /var/log/ | tail -n1  | awk '{ print $5 }' | sed 's/%//')"
+  test "${percent}" -gt "0" || return 1 # sanity check
+  echo "==> current disk usage: ${percent}%"
+  test "${percent}" -gt "65" && return 0
+}
+
 while true; do
   echo "=> about to remove the following elements, which are older than ${1} days old:"
   find /var/log -type f -mtime "+${1}" -ls -delete
   find /var/log -mindepth 1 -empty -ls -delete
   echo "=> about to compress the following logfiles, which are older than 1 day:"
   find /var/log -type f -mtime "+1" -name "*.log" -ls -exec gzip -9 {} \;
+  echo "=> about to check disk usage:"
+  if check_du; then
+    echo "==> above threshold (65%), triggering cleanup"
+    for i in $(seq 1 $1 | tac); do
+      if check_du; then
+        echo "==> due to space constraints, removing the following elements, which are older than ${i} days old:"
+        find /var/log -type f -mtime "+${i}" -ls -delete
+      fi
+    done
+  fi
   echo "=> going back to sleep for one hour..."
   sleep 3600
 done
