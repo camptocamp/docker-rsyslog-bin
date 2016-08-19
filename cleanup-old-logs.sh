5a436d4dfd6156f77536e8908e43c77e8f1bf197
Marc Fournier <marc.fournier@camptocamp.com>
2016-08-19 15:22:03 +0200

    compress logfiles older than 1 day

--- cleanup-old-logs.sh
+++ cleanup-old-logs.sh
@@ -9,6 +9,8 @@ while true; do
   echo "=> about to remove the following elements, which are older than ${1} days old:"
   find /var/log -type f -mtime "+${1}" -ls -delete
   find /var/log -mindepth 1 -empty -ls -delete
+  echo "=> about to compress the following logfiles, which are older than 1 day:"
+  find /var/log -type f -mtime "+1" -name "*.log" -ls -exec gzip -9 {} \;
   echo "=> going back to sleep for one hour..."
   sleep 3600
 done
